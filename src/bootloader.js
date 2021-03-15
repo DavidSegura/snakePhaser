200ee26e3fea7376f4cbfa1d093f6c2fdf9257c2
arges <wolf.arges@gmail.com>
2021-03-15 10:58:43 +0100

    Update - Phaser 3.53.1 cursor keyboard

--- src/bootloader.js
+++ src/bootloader.js
@@ -7,9 +7,9 @@ export class Bootloader extends Phaser.Scene{
         console.log("preload BOOTLOADER");
 
         /* Load images assets */
-        this.load.image("cuerpo", "./assets/body.png");
-        this.load.image("comida", "./assets/food.png");
-        this.load.image("tablero", "./assets/tablero.png");
+        this.load.image("cuerpo", "../assets/body.png");
+        this.load.image("comida", "../assets/food.png");
+        this.load.image("tablero", "../assets/tablero.png");
 
         this.load.on("complete", () =>{
             this.scene.start("play");
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -4,33 +4,34 @@ export class Play extends Phaser.Scene {
     constructor() {
         super( { key: 'play' } );
     }
+    
     preload() {
         console.log('Escena play');
         this.snake = new Snake(this);
     }
+
     create() {
         console.log("create Play");
-        this.input.keyboard.on('keydown_RIGHT', () => {
-            console.log("RIGHT");
-            this.snake.changeMov('derecha');
-        });
-        this.input.keyboard.on('keydown_LEFT', () => {
-            console.log("LEFT");
-            this.snake.changeMov('izquierda');
-        });
-        this.input.keyboard.on('keydown_UP', () => {
-            console.log("UP");
-            this.snake.changeMov('arriba');
-        });
-        this.input.keyboard.on('keydown_DOWN', () => {
-            console.log("DOWN");
-            this.snake.changeMov('abajo');
-        });
-        this.input.keyboard.on('keydown_A', function (event) {
-            console.log('Hello from the A Key!');
-        });
+        this.cursors = this.input.keyboard.createCursorKeys();
     }
+
     update(time) {
         this.snake.update(time);
+
+        if(this.cursors.left.isDown){
+            this.snake.changeMov('izquierda');
+        }
+
+        if(this.cursors.right.isDown){
+            this.snake.changeMov('derecha');
+        }
+
+        if(this.cursors.up.isDown){
+            this.snake.changeMov('arriba');
+        }
+
+        if(this.cursors.down.isDown){
+            this.snake.changeMov('abajo');
+        }
     }
 }
